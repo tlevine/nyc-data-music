2d278227b88c50a6f5ea883555229ef0ebc8a6a3
Thomas Levine <occurrence@thomaslevine.com>
2013-06-11 12:20:56 -0700

    more types

--- data.r
+++ data.r
@@ -26,7 +26,19 @@ FROM dataset;',
   )
 )
 
+# Most recent date
+today <- as.POSIXct(max(datasets[c("createdAt", "publicationDate", "viewLastModified", "rowsUpdatedAt")], na.rm = T), origin = '1970-01-01')
+
+# Datetimes
 datasets[c("createdAt", "publicationDate", "viewLastModified", "rowsUpdatedAt")] <-
   as.data.frame(lapply(datasets[c("createdAt", "publicationDate", "viewLastModified", "rowsUpdatedAt")], function(vec) {
     as.POSIXct(vec, origin = '1970-01-01')
   }))
+
+# Days
+datasets[c("createdAt.day", "publicationDate.day", "viewLastModified.day", "rowsUpdatedAt.day")] <-
+  lapply(datasets[c("createdAt", "publicationDate", "viewLastModified", "rowsUpdatedAt")], as.Date)
+
+# Weeks 
+datasets[c("createdAt.week", "publicationDate.week", "viewLastModified.week", "rowsUpdatedAt.week")] <-
+  lapply(datasets[c("createdAt", "publicationDate", "viewLastModified", "rowsUpdatedAt")], strftime, format = '%Y-%U')
