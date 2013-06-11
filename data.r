9de52e9c39d286ed3725ea4bf50690b988f29947
Thomas Levine <occurrence@thomaslevine.com>
2013-06-11 12:11:33 -0700

    R types

--- data.r
+++ data.r
@@ -15,7 +15,18 @@ SELECT
   
   -- Ownership
   "tableAuthor.screenName", "owner.roleName"
-FROM dataset;
-', dbname = 'appgen.db')
+FROM dataset;',
+  dbname = 'appgen.db',
+  method = c(
+    'character', 'character', 'factor',
+    'factor', 'numeric',
+    'numeric', 'numeric', 'numeric', 'numeric',
+    'numeric', 'numeric',
+    'factor', 'factor'
+  )
+)
 
-datasets
+datasets[c("createdAt", "publicationDate", "viewLastModified", "rowsUpdatedAt")] <-
+  as.data.frame(lapply(datasets[c("createdAt", "publicationDate", "viewLastModified", "rowsUpdatedAt")], function(vec) {
+    as.POSIXct(vec, origin = '1970-01-01')
+  }))
